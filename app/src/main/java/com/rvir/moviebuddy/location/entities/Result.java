05531c0252289e739cd926f11baf4277b7a6d940
Unknown <manica.abramenko@student.um.si>
2019-01-17 16:00:54 +0100

    getters setter (sori pozabla dodat)

--- app/src/main/java/com/rvir/moviebuddy/location/entities/Result.java
+++ app/src/main/java/com/rvir/moviebuddy/location/entities/Result.java
@@ -29,5 +29,70 @@ public class Result implements Serializable {
     @SerializedName("place_id")
     private String placeId;
 
+    @SerializedName("types")
+    private List<String> types = new ArrayList<>();
 
+    public Geometry getGeometry() {
+        return geometry;
+    }
+
+    public void setGeometry(Geometry geometry) {
+        this.geometry = geometry;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public OpeningHours getOpeningHours() {
+        return openingHours;
+    }
+
+    public void setOpeningHours(OpeningHours openingHours) {
+        this.openingHours = openingHours;
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
+    }
+
+    public String getPlaceId() {
+        return placeId;
+    }
+
+    public void setPlaceId(String placeId) {
+        this.placeId = placeId;
+    }
+
+    public List<String> getTypes() {
+        return types;
+    }
+
+    public void setTypes(List<String> types) {
+        this.types = types;
+    }
 }
