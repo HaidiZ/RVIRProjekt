98f3b4594eca06e8b141723c0b8c872b5979933b
Unknown <manica.abramenko@student.um.si>
2019-01-18 20:12:02 +0100

    nadaljevanje maps... počasi bo nekaj :D

---
+++ app/src/main/java/com/rvir/moviebuddy/location/services/GoogleMapAPI.java
@@ -0,0 +1,19 @@
+package com.rvir.moviebuddy.location.services;
+
+import com.rvir.moviebuddy.location.entities.Result;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface GoogleMapAPI {
+
+    @GET("place/nearbysearch/json")
+    Call<Result> getNearBy(
+        @Query("location") String location,
+        @Query("radius") int radius,
+        @Query("type") String type,
+        @Query("keyword") String keyword,
+        @Query("key") String key
+    );
+}
