8b5f94441f03cda642393440511683c6072ac0b7
Unknown <manica.abramenko@student.um.si>
2019-01-19 21:13:11 +0100

    Maps , mislim da končano
    .....

--- app/src/main/java/com/rvir/moviebuddy/location/MapsActivity.java
+++ app/src/main/java/com/rvir/moviebuddy/location/MapsActivity.java
@@ -1,16 +1,31 @@
 package com.rvir.moviebuddy.location;
 import com.rvir.moviebuddy.R;
-
-        import androidx.appcompat.app.AppCompatActivity;
-
-        import android.os.Bundle;
+import com.rvir.moviebuddy.location.adapters.PlacesListAdapter;
+import com.rvir.moviebuddy.location.entities.PlacesResults;
+import com.rvir.moviebuddy.location.entities.Result;
+import com.rvir.moviebuddy.location.services.APIClient;
+import com.rvir.moviebuddy.location.services.GoogleMapAPI;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
-
+import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +35,8 @@ public class MapsActivity extends AppCompatActivity {
     private Button buttonSearch;
     private ListView listViewPlaces;
     private Spinner spinnerType;
+
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +56,97 @@ public class MapsActivity extends AppCompatActivity {
     }
 
     private void buttonSearch_onClick(View view) {
+        /*String keyword = editTextKeyword.getText().toString();
+        String key = getText(R.string.google_maps_key).toString();
+        String location ="46.554649,15.645881";
+        int radius = 1500;
+        String type = spinnerType.getSelectedItem().toString();
+        GoogleMapAPI googleMapAPI = APIClient.getClient().create(GoogleMapAPI.class);
+        googleMapAPI.getNearBy(location, radius, type, keyword, key).enqueue(new Callback<PlacesResults>() {
+            @Override
+            public void onResponse(Call<PlacesResults> call, Response<PlacesResults> response) {
+                if(response.isSuccessful()) {
+                    List<Result> results = response.body().getResults();
+                    PlacesListAdapter placesListAdapter = new PlacesListAdapter(
+                            getApplicationContext(), results);
+                    listViewPlaces.setAdapter(placesListAdapter);
+                }else {
+                    Toast.makeText(getApplicationContext(), "Faileed", Toast.LENGTH_LONG).show();
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PlacesResults> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }); */
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2 * 1000, 1, locationListener);
+            } else {
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 12);
+                }
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION}, 13);
+                }
+                }
+        }
 
 
-    }
+
+
+        private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            String keyword = editTextKeyword.getText().toString();
+            String key = getText(R.string.google_maps_key).toString();
+            String currentLocation = location.getLatitude() + "," + location.getLongitude();
+            int radius = 1500;
+            String type = spinnerType.getSelectedItem().toString();
+            GoogleMapAPI googleMapAPI = APIClient.getClient().create(GoogleMapAPI.class);
+            googleMapAPI.getNearBy(currentLocation, radius, type, keyword, key).enqueue(new Callback<PlacesResults>() {
+                @Override
+                public void onResponse(Call<PlacesResults> call, Response<PlacesResults> response) {
+                    if(response.isSuccessful()) {
+                        List<Result> results = response.body().getResults();
+                        PlacesListAdapter placesListAdapter = new PlacesListAdapter(
+                                getApplicationContext(), results);
+                        listViewPlaces.setAdapter(placesListAdapter);
+                    }else {
+                        Toast.makeText(getApplicationContext(), "Faileed", Toast.LENGTH_LONG).show();
+
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<PlacesResults> call, Throwable t) {
+                    Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
     private void loadData(){
 
--- app/src/main/java/com/rvir/moviebuddy/location/adapters/PlacesListAdapter.java
+++ app/src/main/java/com/rvir/moviebuddy/location/adapters/PlacesListAdapter.java
@@ -1,17 +1,26 @@
 package com.rvir.moviebuddy.location.adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.rvir.moviebuddy.R;
 
 import com.rvir.moviebuddy.location.entities.Result;
 
+import java.io.InputStream;
 import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class PlacesListAdapter extends ArrayAdapter<Result> {
 
@@ -26,7 +35,50 @@ public class PlacesListAdapter extends ArrayAdapter<Result> {
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+    public View getView(int position, @Nullable View view, @NonNull ViewGroup parent) {
+        //return super.getView(position, convertView, parent);
+        try {
+            ViewHolder viewHolder;
+            if (view == null){
+                viewHolder = new ViewHolder();
+                view = LayoutInflater.from(context).inflate(R.layout.place_row_layout, null);
+                viewHolder.textViewName = view.findViewById(R.id.textViewName);
+                viewHolder.textViewAddress = view.findViewById(R.id.textViewAddress);
+                viewHolder.imageViewPhoto = view.findViewById(R.id.imageViewPhoto);
+                view.setTag(viewHolder);
+            }else {
+                viewHolder = (ViewHolder) view.getTag();
+            }
+            Result result = results.get(position);
+            viewHolder.textViewName.setText(result.getName());
+            viewHolder.textViewAddress.setText(result.getVicinity());
+            Bitmap bitmap = new ImageAsynTask().doInBackground(result.getIcon());
+            viewHolder.imageViewPhoto.setImageBitmap(bitmap);
+            return view;
+
+        }catch (Exception e){
+            return null;
+        }
     }
+
+    public static class ViewHolder{
+        public  TextView textViewName;
+        public  TextView textViewAddress;
+        public  ImageView imageViewPhoto;
+
+    }
+
+    private class ImageAsynTask extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            try{
+                InputStream inputStream = new java.net.URL(params[0]).openStream();
+                return BitmapFactory.decodeStream(inputStream);
+            }catch (Exception e){
+                return null;
+            }
+        }
+    }
+
 }
