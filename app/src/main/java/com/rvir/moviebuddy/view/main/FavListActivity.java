cdbaec71a67449b4f901e9c8898c07919ac438a8
Haidi <zakelsek.haidi@gmail.com>
2019-01-21 22:46:33 +0100

    Popravljen recycle view.

--- app/src/main/java/com/rvir/moviebuddy/view/main/FavListActivity.java
+++ app/src/main/java/com/rvir/moviebuddy/view/main/FavListActivity.java
@@ -8,7 +8,10 @@ import androidx.room.Room;
 
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Toast;
+
 import com.rvir.moviebuddy.R;
 import com.rvir.moviebuddy.dao.AppDatabase;
 import com.rvir.moviebuddy.entity.Favourite;
@@ -20,15 +23,17 @@ public class FavListActivity extends AppCompatActivity {
 
     RecyclerView mRecyclerView;
     RecyclerView.Adapter mAdapter;
-
+    AppDatabase myAppDatabase;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fav_list);
+        myAppDatabase = Room.databaseBuilder(this, AppDatabase.class, "Favourite").build();
+        populateRecyclerView();
 
-        AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "production").allowMainThreadQueries().build();
+       /* AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "production").allowMainThreadQueries().build();
 
         List<Favourite> favList = db.favouriteDao().getAll();
 
@@ -37,6 +42,31 @@ public class FavListActivity extends AppCompatActivity {
         FavouriteAdapter favAdapter = new FavouriteAdapter((Context) favList, this);
         recyclerView.setAdapter(favAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+*/
+    }
 
+    private void populateRecyclerView() {
+        new GetSeznamFavAsyncTask().execute();
+    }
+
+    public class GetSeznamFavAsyncTask  extends AsyncTask<Void, Integer, List<Favourite>> {
+
+        @Override
+        protected List<Favourite> doInBackground(Void... voids) {
+            return FavListActivity.this.myAppDatabase.favouriteDao().getAll();
+        }
+
+        @Override
+        protected void onPostExecute(List<Favourite> osebe) {
+            super.onPostExecute(osebe);
+            if(osebe.size() ==  0) {
+                return;
+            }
+            RecyclerView recyclerView = findViewById(R.id.favrecycleview);
+            FavouriteAdapter zaposleniAdapter = new FavouriteAdapter(osebe);
+            recyclerView.setAdapter(zaposleniAdapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(FavListActivity.this));
+            Toast.makeText(getApplicationContext(), "seznam posodobljen", Toast.LENGTH_SHORT).show();
+        }
     }
 }
